b3e9631b7398f400eadbd7deca2d84ee03a674c7
BloomBabe <schebnev1998@gmail.com>
2021-02-08 18:48:04 +0000

    Add PointNet module

--- model.py
+++ model.py
@@ -9,11 +9,14 @@ def conv1d_bn(input_channels, output_channels):
                     nn.ReLU(inplace=True)]
     return nn.Sequential(*conv1d_layer)
 
-def fc_bn(input_channels, output_channels):
-    fc_layer = [nn.Linear(input_channels, output_channels),
-                nn.BatchNorm1d(output_channels),
-                nn.ReLU(inplace=True)]
+def fc_bn(input_channels, output_channels, dropout=False, p=0.3):
+    fc_layer = [nn.Linear(input_channels, output_channels)]
+    if dropout:
+        fc_layer.append(nn.Dropout(p=0.3))
+    fc_layer.append(nn.BatchNorm1d(output_channels))
+    fc_layer.append(nn.ReLU(inplace=True))
     return nn.Sequential(*fc_layer)
+
 
 class TNet(nn.Module):
     
@@ -47,13 +50,62 @@ class TNet(nn.Module):
         output = self.fc3(output)
 
         iden = torch.eye(self.channel, requires_grad=True). \
-                    repeat(self.batch_size, 1)
+                     repeat(self.batch_size, 1)
 
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         iden = iden.to(device)
         output += iden
 
-        return output.view(self.batch_size, self.channel, self.channel) 
+        matrix_KxK = output.view(self.batch_size, self.channel, self.channel)
+        output =  torch.bmm(torch.transpose(x, 1, 2), matrix_KxK).transpose(1, 2)
+
+        return  matrix_KxK, output
+
+
+class PointNet(nn.Module):
+
+    def __init__(self, 
+                 num_classes = 10,
+                 channel = 3):
+        super(PointNet, self).__init__()
+        self.channel = channel
+        self.num_classes = num_classes
+
+        self.input_transform = TNet(self.channel)
+        self.conv1_bn = conv1d_bn(self.channel, 64)
+
+        self.feature_transform = TNet(64)
+        self.conv2_bn = conv1d_bn(64, 128)
+        self.conv3_bn = conv1d_bn(128, 1024)
+        self.max_pool = nn.MaxPool1d(1024)
+
+        self.fc1_bn = fc_bn(1024, 512)
+        self.fc2_bn = fc_bn(512, 256, dropout=True)
+
+        self.fc3 = nn.Linear(256, num_classes)
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+    
+
+    def forward(self, x): 
+        matrix3x3, output = self.input_transform(x)
+        output = self.conv1_bn(output)
+
+        matrix64x64, output = self.feature_transform(output)
+        output = self.conv2_bn(output)
+        output = self.conv3_bn(output)
+        output = self.max_pool(output)
+        output = nn.Flatten()(output)
+
+        output = self.fc1_bn(output)
+        output = self.fc2_bn(output)
+        output = self.fc3(output)
+
+        pred = self.logsoftmax(output)
+        return pred, matrix3x3, matrix64x64
+
+        
+
+
 
 
 
